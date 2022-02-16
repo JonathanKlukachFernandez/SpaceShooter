4d294ff2034a1d11f535d405d0a2e28cb4f64df9
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2022-02-16 14:19:53 +0100

    added project

---
+++ Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagedByCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D()
+    {
+        Debug.Log("Collision!");
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger!");
+    }
+}
